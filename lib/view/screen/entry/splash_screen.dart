1e2edd8427d624ae74558bdf190a92903d3c17c0
AparnaRajVR <aparnaraajvr@gmail.com>
2025-04-19 13:56:00 +0530

    after merge with auth conflit

--- lib/view/screen/entry/splash_screen.dart
+++ lib/view/screen/entry/splash_screen.dart
@@ -1,9 +1,51 @@
-// import 'package:flutter/material.dart';
-// // import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:get/get.dart';
+// // import 'package:flutter/material.dart';
+// // // import 'package:firebase_auth/firebase_auth.dart';
+// // import 'package:get/get.dart';
 
-// // import 'package:zesta_1/view/screen/entry/login_screen.dart';
-// import 'package:zesta_1/view/screen/entry/welcome.dart';
+// // // import 'package:zesta_1/view/screen/entry/login_screen.dart';
+// // import 'package:zesta_1/view/screen/entry/welcome.dart';
+
+
+
+// // class SplashScreen extends StatelessWidget {
+// //   const SplashScreen({super.key});
+
+// //   void _navigate(BuildContext context) {
+// //     // final FirebaseAuth auth = FirebaseAuth.instance;
+// //     // final User? user = auth.currentUser;
+
+// //      Get.off(()=>WelcomeScreen());
+// //     //.................................
+
+// //     // if (user != null) {
+// //     //   Get.off(() => Dashboard());
+// //     // } else {
+// //     //   Get.off(() => OnboardingScreen());
+// //     // }
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     Future.delayed(const Duration(seconds: 2), () => _navigate(context));
+
+// //     return Scaffold(
+// //       body: Center(
+// //         child: Image.asset(
+// //           'assets/logo.png',
+// //           height: 150,
+// //           width: 150,
+// //         ),
+// //       ),
+// //     );
+// //   }
+// // }
+
+// import 'package:flutter/material.dart';
+//  import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:get/get.dart';
+// import 'package:zesta_1/view/screen/dash_board.dart';
+
+// import 'package:zesta_1/view/screen/entry/onboard.dart';
 
 
 
@@ -11,30 +53,36 @@
 //   const SplashScreen({super.key});
 
 //   void _navigate(BuildContext context) {
-//     // final FirebaseAuth auth = FirebaseAuth.instance;
-//     // final User? user = auth.currentUser;
+//     final FirebaseAuth auth = FirebaseAuth.instance;
+//     final User? user = auth.currentUser;
 
-//      Get.off(()=>WelcomeScreen());
+//     //  Get.off(()=>WelcomeScreen());
 //     //.................................
 
-//     // if (user != null) {
-//     //   Get.off(() => Dashboard());
-//     // } else {
-//     //   Get.off(() => OnboardingScreen());
-//     // }
+//     if (user != null) {
+//       Get.off(() => Dashboard());
+//     } else {
+//       Get.off(() => OnboardingScreen());
+//     }
 //   }
 
 //   @override
 //   Widget build(BuildContext context) {
-//     Future.delayed(const Duration(seconds: 2), () => _navigate(context));
-
 //     return Scaffold(
-//       body: Center(
-//         child: Image.asset(
-//           'assets/logo.png',
-//           height: 150,
-//           width: 150,
-//         ),
+//       body: FutureBuilder<User?>(
+//         future: checkUserStatus(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return Center(child: CircularProgressIndicator());
+//           } else {
+//             // Navigate to the next screen once data is ready
+//             WidgetsBinding.instance.addPostFrameCallback((_) {
+//               navigateToNextScreen(snapshot.data);
+//             });
+
+//             return Center(child: Image.asset('assets/logo.png', height: 150, width: 150));
+//           }
+//         },
 //       ),
 //     );
 //   }
@@ -68,21 +116,15 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<User?>(
-        future: checkUserStatus(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            // Navigate to the next screen once data is ready
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              navigateToNextScreen(snapshot.data);
-            });
+    Future.delayed(const Duration(seconds: 2), () => _navigate(context));
 
-            return Center(child: Image.asset('assets/logo.png', height: 150, width: 150));
-          }
-        },
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          height: 150,
+          width: 150,
+        ),
       ),
     );
   }
